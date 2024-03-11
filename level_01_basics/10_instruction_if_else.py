5cba686d34f3a29ee761579061ce0f87f2b3c9a5
Luka <lukalutumba@hotmail.com>
2024-03-11 15:50:00 +0100

    Add script: Instruction If-Else

---
+++ level_01_basics/10_instruction_if_else.py
@@ -0,0 +1,13 @@
+note = 12
+
+if note >= 10:
+    print("Admis")
+else:
+    print("Ajourné")
+
+# Test avec une autre valeur
+# note = 8
+# if note >= 10:
+#     print("Admis")
+# else:
+#     print("Ajourné") 
