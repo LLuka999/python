c0c909482b7ed82c3ccb87a4c05ecdfe200472db
Luka <lukalutumba@hotmail.com>
2024-03-25 09:10:00 +0100

    Add script: Boucle For avec Range

---
+++ level_01_basics/15_boucle_for_range.py
@@ -0,0 +1,11 @@
+print("Affichage des nombres de 0 à 4:")
+for i in range(5):
+    print(f"Nombre: {i}")
+
+print("\nAffichage des nombres de 2 à 6 (exclus):")
+for j in range(2, 6):
+    print(f"Nombre: {j}")
+
+print("\nAffichage des nombres de 1 à 10 par pas de 2:")
+for k in range(1, 11, 2):
+    print(f"Nombre: {k}") 
