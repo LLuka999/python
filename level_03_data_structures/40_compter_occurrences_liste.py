02f3f328d86a1572c03b0db919ab450fa8d98919
Luka <lukalutumba@hotmail.com>
2024-05-13 13:45:00 +0200

    Add script: Compter Occurrences Liste

---
+++ level_03_data_structures/40_compter_occurrences_liste.py
@@ -0,0 +1,5 @@
+ma_liste = [1, 2, 3, 2, 4, 2, 5]
+element = 2
+print(f"La liste : {ma_liste}")
+nb_occurrences = ma_liste.count(element)
+print(f"L'élément {element} apparaît {nb_occurrences} fois.") 
